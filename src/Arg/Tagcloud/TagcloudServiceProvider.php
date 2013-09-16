d26c6da04087dd3ef6dece99670985c72288bc84
aleksandr <aleksandr@arc.poly.edu>
2013-09-16 17:34:07 -0400

    Initial Commit

---
+++ src/Arg/Tagcloud/TagcloudServiceProvider.php
@@ -0,0 +1,38 @@
+<?php namespace Arg\Tagcloud;
+
+use Illuminate\Support\ServiceProvider;
+
+class TagcloudServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->booting(function()
+		{
+			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+			$loader->alias('Tagcloud', 'Arg\Tagcloud\Tagcloud');
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
